b236d4983dc2017e7a7799dfe8b8c3d8d64333e6
Mathman <70949660+17e23052@users.noreply.github.com>
2021-10-05 10:55:50 +0000

    A1 C1 comments

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 word = "sandwich"
 guess = ""
-not_done = word != guess
+not_done = word != guess #not done when the guess does not equal the word
 guesses = 0
 
-while not_done and guesses < 5:
+while not_done and guesses < 5: #if the word is not guessed and less than 5 guesses
   guess = input("Guess the word: ")
   if guess == word:
     guesses += 1
     correct = "CORRECT!"
-    not_done = False
+    not_done = False #if guessed correctly, the loop finishes
   else:
     guesses += 1
     correct = "INCORRECT!"
