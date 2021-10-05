843902e4d39a0d13d98a384abc3f3d8e1317fdab
Mathman <70949660+17e23052@users.noreply.github.com>
2021-10-05 11:05:24 +0000

    A1 C2 comments

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from random import choice
 from time import sleep
 
-simon_says = []
+simon_says = [] #starts the list of instructions
 adding_instructions = True
 
 while adding_instructions:
     instruction = input("Enter a Simon says instruction: ")
-    simon_says.append(instruction)
+    simon_says.append(instruction) #appends the instruction to the 'simon_says' list
     answer = input("Would you like to add another? Y/N ").upper()
     if answer == "N":
-        adding_instructions = False
+        adding_instructions = False #the while loop ends if no more inst. are requested
 
 intros =  ["Simon says...", ""]
 
 for x in range(len(simon_says)):
     print(choice(intros), simon_says[x])
     sleep(3)
+#prints either 'Simon says...' or nothing, then the instruction. It waits before executing the next one, and repeating until all instructions are shown.
